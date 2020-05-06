83ac9f9c2adef217444b2f4b0cfec3d7f04c1e85
Victor <deep-research>
2020-05-06 15:54:48 -0500

    Title component created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SiteTitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SiteTitle from './components/SiteTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SiteTitle
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
+
+  body {
+    margin: 0;
+  }
 </style>
---
+++ frontend/src/components/SiteTitle.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="site-title">
+    <p id="title">
+      Victor's Library
+    </p>
+    <div id="subtitle">
+      Research on Music and Religion
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteTitle'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import
+    url('https://fonts.googleapis.com/css2?family=Gilda+Display&family=Merienda+One&display=swap');
+
+  #title { 
+    font-family: 'Merienda One', cursive;
+    font-size: 48px;
+    line-height: 150%;
+    margin: 0;
+  }
+
+  #subtitle {
+    font-family: 'Gilda Display', serif;
+    font-weight: 600;
+    font-size: 22px;
+  }
+
+  .site-title {
+    margin-bottom: 40px;
+    margin-top: 40px;
+  }
+
+  @media only screen and (max-width: 700px) {
+    #title {
+      outline: 0;
+      transition: all .1s linear;
+      transition-property: all;
+      transition-duration: 0.1s;
+      transition-timing-function: linear;
+      transition-delay: 0s;
+      font-size: 29px;
+    }
+    #subtitle {
+      font-size: 14px;
+    }
+  }
+</style>
