1e6d0f619664caf585fa26cdf5bdb85646531da6
Victor <deep-research>
2020-06-14 16:16:23 -0500

    Extra Scroll Method Removed

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -66,46 +66,6 @@
 </template>
 
 <script>
-// scroll to top (0) in 4 seconds e some milliseconds
-scrollTo(0, 4269);
-
-// Element to move, time in ms to animate
-function scrollTo(element, duration) {
-    var e = document.documentElement;
-    if(e.scrollTop===0){
-        var t = e.scrollTop;
-        ++e.scrollTop;
-        e = t+1===e.scrollTop--?e:document.body;
-    }
-    scrollToC(e, e.scrollTop, element, duration);
-}
-
-// Element to move, element or px from, element or px to, time in ms to animate
-function scrollToC(element, from, to, duration) {
-    if (duration <= 0) return;
-    if(typeof from === "object")from=from.offsetTop;
-    if(typeof to === "object")to=to.offsetTop;
-
-    scrollToX(element, from, to, 0, 1/duration, 20, easeOutCuaic);
-}
-
-function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
-    if (t01 < 0 || t01 > 1 || speed<= 0) {
-        element.scrollTop = xTo;
-        return;
-    }
-    element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
-    t01 += speed * step;
-
-    setTimeout(function() {
-        scrollToX(element, xFrom, xTo, t01, speed, step, motion);
-    }, step);
-}
-function easeOutCuaic(t){
-    t--;
-    return t*t*t+1;
-}
-
 import Navbar from '../components/Navbar.vue'
 
 export default {
